35214af526d22eeb50b4524197d8cfa68a3446d4
Bruno Silveira <dasilveira.bru@terra.com.br>
2023-05-01 17:04:02 -0300

    v1 complete ready

--- src/App.js
+++ src/App.js
@@ -6,29 +6,51 @@ import { useState } from "react";
 
 function App() {
   
+  const GameStates = {
+    SET_SCORE: 1,
+    GAMELPLAY: 2,
+  }
+
+  const [gameState, setGameState] = useState(GameStates.SET_SCORE)
   const [p1Score, setP1Score] = useState(0)
   const [p2Score, setP2Score] = useState(0)
+  const [maxScore, setMaxScore] = useState(24)
   const [currTimerId, setCurrTimerId] = useState(null)
   const [fastIncrement, setFastIncrement] = useState(0)
   const [mandrakeP1Img, setMandrakeP1Img] = useState(mandrakeNormalImg)
   const [mandrakeP2Img, setMandrakeP2Img] = useState(mandrakeNormalImg)
+  const [victorySound, setVictorySound] = useState(new Audio("./sounds/win_song.mp3"))
 
   const mandrakeSound = new Audio("./sounds/mandrake_scream.mp3")
   const score4Sound = new Audio("./sounds/avada_kedavra.mp3")
-  //const victorySound = new Audio("./sounds/win_song.mp3")
+  const score1Sound = new Audio("./sounds/spell_z.mp3")
+  const score2Sound = new Audio("./sounds/spell_a.mp3")
+  const score3Sound = new Audio("./sounds/spell_b.mp3")
+
 
   const SCORE_DELAY_MS = 500
+
+  function startPlaying() {
+    setGameState(GameStates.GAMELPLAY)
+    setP1Score(0)
+    setP2Score(0)
+    victorySound.pause()
+    victorySound.currentTime = 0;
+  }
 
   function endTimer(fastIncValue) {
     switch(fastIncValue) {
       case 1:
         console.log("PLAYED 1 SOUND")
+        score1Sound.play()
         break
       case 2:
         console.log("PLAYED 2 SOUND")
+        score2Sound.play()
         break
       case 3:
         console.log("PLAYED 3 SOUND")
+        score3Sound.play()
         break
       case 4:
         console.log("PLAYED 4 SOUND")
@@ -40,8 +62,16 @@ function App() {
     setFastIncrement(0)
   }
 
+  function endGame() {
+    setGameState(GameStates.SET_SCORE)
+    victorySound.play()
+  }
+
   function increaseP1Score(increment) {
-    setP1Score(p1Score+increment)
+    if (p1Score+increment >= maxScore) {
+      endGame()
+    }
+    setP1Score(p1Score => p1Score+increment)
     if (increment > 1) {
       return
     }
@@ -58,7 +88,10 @@ function App() {
 
   }
   function increaseP2Score(increment) {
-    setP2Score(p2Score+increment)
+    if (p2Score+increment >= maxScore) {
+      endGame()
+    }
+    setP2Score(p2Score => p2Score+increment)
     if (increment > 1) {
       return
     }
@@ -87,8 +120,23 @@ function App() {
     setMandrakeP2Img(mandrakeNormalImg)
   }
 
+  function hasAWinner() {
+    return maxScore !== null && (p1Score >= maxScore || p2Score >= maxScore)
+  }
+
   return (
     <>
+      { gameState === GameStates.SET_SCORE && 
+        <div className="backImg" style={{ backgroundImage: `url(${backgroundImg})` }}>
+          <div className="menu">
+            {hasAWinner() && <h3 className="winnerText">Jogador {p1Score >= maxScore ? "1" : "2"} venceu!</h3>}
+            <label htmlFor="maxScoreInput" className="inputLabel">Pontuacao maxima:</label>
+            <input type="number" id="maxScoreInput" value={maxScore} onChange={(e => setMaxScore(Number(e.target.value)))}/>
+            <button className="playButton" onClick={startPlaying}>Jogar</button>
+          </div>
+        </div>
+      }
+      { gameState === GameStates.GAMELPLAY &&
       <div className="backImg" style={{ backgroundImage: `url(${backgroundImg})` }}>
         <div className="p1Area">
           <button className="counterButton" onClick={() => increaseP1Score(1)}>{p1Score}</button>
@@ -99,6 +147,7 @@ function App() {
         <img src={mandrakeP1Img} className="mandrakeLeft" onClick={mandrakeP1} alt="Mandrake"/>
         <img src={mandrakeP2Img} className="mandrakeRight" onClick={mandrakeP2} alt="Mandrake"/>
       </div>
+      }
     </>
   )
 }
